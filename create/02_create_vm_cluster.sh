6cec25a297ae1f4c8a15e6254b60f237bdb45dee
Iresh Madhusanka <socialexpz1@gmail.com>
2021-01-12 16:34:25 +0000

    finish off create_vm method add line to create ocp.lan network for the cluster create control plane and compute nodes add corresponding destroy commands

--- create/02_create_vm_cluster.sh
+++ create/02_create_vm_cluster.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 vcd vapp create -d 'VM Cluster for OCP cluster' ocp-cluster
+vcd network isolated create \
+              -g 192.168.22.1 \
+              -n 255.255.255.0 \
+              --dhcp-disabled \
+              ocp.lan
+
+vcd vapp network create-ovdc-network ocp-cluster ocp.lan
 
 # $1 -> Cluster name
 # $2 -> VM name
@@ -9,7 +16,6 @@ vcd vapp create -d 'VM Cluster for OCP cluster' ocp-cluster
 # $5 -> Disk size
 # $6 -> Media <catalog> <media_name>
 create_vm() {
-
 vcd vapp add-vm-scratch -vm "$2"\
                         -cn "$2"\
                         -os 'coreos64Guest' \
@@ -25,10 +31,26 @@ vcd vapp attach "$1" "$2" "$2"-disk
 
 vcd vm insert-cd \
     --media-id `vcd -j catalog info $6 | jq -r '."template-id"'` \
-    ocp-cluster \
-    ocp-bootstrap
+    "$1" \
+    "$2"
 
+vcd vm add-nic \
+            --adapter-type VMXNET3 \
+            --network ocp.lan \
+            --primary \
+            --connect \
+            "$1" \
+            "$2"
 }
 
-
 create_vm "ocp-cluster" "ocp-bootstrap" 6 6 40 "iso rhcos.iso"
+
+# Create control plane
+for instance in {1..3}; do
+  create_vm "ocp-cluster" "ocp-cp-${instance}" 4 8 50 "iso rhcos.iso"
+done
+
+# Create compute nodes
+for instance in {1..2}; do
+  create_vm "ocp-cluster" "ocp-w-${instance}" 4 8 50 "iso rhcos.iso"
+done
--- destroy/02_destroy_vm_cluster.sh
+++ destroy/02_destroy_vm_cluster.sh
@@ -2,4 +2,14 @@
 
 vcd vapp delete -y -f ocp-cluster
 
+vcd network isolated delete -y ocp.lan
+
 vcd disk delete -y ocp-bootstrap-disk
+
+for instance in {1..3}; do
+  vcd disk delete -y "ocp-cp-${instance}-disk"
+done
+
+for instance in {1..2}; do
+  vcd disk delete -y "ocp-w-${instance}-disk"
+done
