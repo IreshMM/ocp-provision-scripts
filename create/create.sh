8f7bc9b99fde7f7fbeb9c03df2370c43196f28ce
Iresh Madhusanka <socialexpz1@gmail.com>
2021-01-16 02:07:00 +0000

    add create.sh and destroy.sh to call all corresponding scripts in order

---
+++ create/create.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./01_create_catalog.sh
+./02_create_vm_cluster.sh
+./03_create_ocp_svc.sh
---
+++ destroy/destroy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./03_destroy_ocp_svc.sh
+./02_destroy_vm_cluster.sh
+./01_delete_catalog.sh
